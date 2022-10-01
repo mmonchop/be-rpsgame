57f9d3fdb45ed3cfafc2e2c5a080d9ad633d3826
MLMO <miguel.moncho@gft.com>
2022-10-01 13:55:05 +0200

    Added new event to control when player leaves window

--- src/main/java/com/mlmo/rpsgame/controller/RoomWSController.java
+++ src/main/java/com/mlmo/rpsgame/controller/RoomWSController.java
@@ -16,8 +16,8 @@ public class RoomWSController {
     @MessageMapping("/rooms/{roomId}/publish")
     @SendTo("/topic/rooms/{roomId}")
     public NotificationEvent getNotificationEvent(NotificationEvent notificationEvent, @DestinationVariable String roomId) {
-        log.info(String.format("Publishing NotificationEvent for id [%s] in folder [%s] to subscribers: ",
-                notificationEvent.getId(), roomId));
+        log.info(String.format("Publishing NotificationEvent [id: %s][type: %s] to [/topic/rooms/%s] subscribers.",
+                notificationEvent.getId(), notificationEvent.getType(), roomId));
         log.finest(notificationEvent.toString());
         return notificationEvent;
     }
